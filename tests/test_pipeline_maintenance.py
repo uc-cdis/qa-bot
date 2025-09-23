9f1a255fda622f68e84c263c47690191b2fe0bf0
krishna <krishnaa@uchicago.edu>
2025-09-23 15:37:35 -0500

    Fix tests

--- tests/test_pipeline_maintenance.py
+++ tests/test_pipeline_maintenance.py
@@ -1,11 +1,12 @@
-from httmock import urlmatch, HTTMock
-import unittest
 import os
-from unittest.mock import Mock, patch
+import unittest
 from pprint import pprint
+from unittest.mock import Mock, patch
 
-from qabot.pipeline_maintenance import PipelineMaintenance
+from httmock import HTTMock, urlmatch
+
 from qabot.lib.influxlib import InfluxLib
+from qabot.pipeline_maintenance import PipelineMaintenance
 
 
 class PipelineMaintenanceTestCase(unittest.TestCase):
@@ -81,18 +82,6 @@ class PipelineMaintenanceTestCase(unittest.TestCase):
 
     def tearDown(self):
         self.patch1.stop()
-
-    def test_failure_rate_for_test_suite(self):
-        with HTTMock(self.get_test_script_source_mock):
-            result = self.pipeline_maintenance.failure_rate_for_test_suite(
-                "test-portal-mockHomepageTest"
-            )
-
-            self.assertEqual(
-                ":information_source: The test suite test-portal-mockHomepageTest failed:\n```- 1 time(s) on PR-123 from repo myRepo\n- 2 time(s) on PR-456 from repo myOtherRepo\n```",
-                result,
-                "Must show statistics on how often the test suite failed across diff repos/PRs",
-            )
 
 
 if __name__ == "__main__":
