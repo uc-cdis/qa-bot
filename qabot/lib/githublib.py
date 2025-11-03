d9e801c053a5bb722025b8d8905f4f379425b775
krishna <krishnaa@uchicago.edu>
2025-11-03 11:04:14 -0600

    Retrieve External Testing PR info

--- qabot/lib/githublib.py
+++ qabot/lib/githublib.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import time
 
 import requests
 from github import Github
@@ -91,3 +92,21 @@ class GithubLib:
             payload["inputs"] = inputs
         response = requests.post(url, headers=headers, json=payload)
         return response
+
+    def get_external_pr_number(self, repo_name, search_title):
+        for i in range(6):
+            time.sleep(10)
+            url = "https://api.github.com/search/issues"
+            query = f"repo:{self.org}/{repo_name}+type:pr+in:title+{search_title}"
+            headers = {
+                "Authorization": f"token {self.token}",
+                "Accept": "application/vnd.github+json",
+            }
+            response = requests.get(url, headers=headers, params={"q": query})
+            response.raise_for_status()
+            items = response.json().get("items", [])
+            if not items:
+                continue
+            pr = items[0]
+            return pr["html_url"]
+        return False
